bc1bdedac1a6e3f91d037d46c0fe964402aaad8f
Serena15 <raketa9390@mail.ru>
2015-11-06 14:12:36 +0300

    Update JSONL.pm

--- JSONL.pm
+++ JSONL.pm
@@ -4,19 +4,15 @@ use JSON;
 use strict;
 use warnings;
 our $VERSION = '1.00';
-
 use Exporter 'import';
 
 our @EXPORT_OK = qw(encode_jsonl decode_jsonl);
 
-sub encode_jsonl{
-	
-	my ($perlscalar) = @_;
-	
-	my @array = @{$perlscalar};
-	
+sub encode_jsonl {
+	my ($array_perl) = @_;
+	my @array = @{$array_perl};
 	my @result = ();
-
+	
 	for my $arg(@array){
 	
 		push @result, encode_json $arg;
@@ -24,17 +20,12 @@ sub encode_jsonl{
 	}
 	
 	my $str = join("\n", @result);
-	
 	return $str;
-
 }
 
-sub decode_jsonl{
-
+sub decode_jsonl {
 	my ($jsontext) = @_;
-	
 	my @array = split("\n", $jsontext);
-	
 	my @result = ();
 	
 	for my $arg(@array){			
@@ -44,5 +35,4 @@ sub decode_jsonl{
 	}	
 	
 	return \@result;
-
 }
